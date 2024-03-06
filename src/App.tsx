adfd17b75beea7b5a79c8a09cbf85e3a62920320
Stefano Calcaterra <37615901+stefano1892@users.noreply.github.com>
2024-03-06 08:55:59 +0100

    start login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*import Register from './pages/Registration';
+import UserProfile from './pages/UserProfile';
+import HomeComponent from './pages/Home';
+import NavigationBarComponent from './components/NavigationBar';
+import UserSettings from './pages/userSettings';*/
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/*<Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<><NavigationBarComponent /><UserProfile /></>} />
+          <Route path="/home" element={<><NavigationBarComponent /><HomeComponent /></>} />
+  <Route path="/settings" element={<><NavigationBarComponent /><UserSettings /></>} />*/}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+const Login = () => {
+    return (
+        <main>
+            <div className="container container-login">
+            <div className="login-card">
+                <input type="text"/>
+            </div>
+            </div>
+            
+        </main>
+    );
+}
+ 
+export default Login;
