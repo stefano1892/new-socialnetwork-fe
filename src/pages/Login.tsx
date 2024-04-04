4f65613134cc53d3c0aa3f0d05a078244e26ac16
Stefano Calcaterra <37615901+stefano1892@users.noreply.github.com>
2024-04-04 22:29:31 +0200

    sistemazione git da qua in poi

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,27 @@
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return (
-        <main>
-            <div className="container container-login">
-            <div className="login-card">
-                <input type="text"/>
-            </div>
-            </div>
-            
-        </main>
-    );
+  return (
+    <main>
+      <div className="container container-login">
+        <div className="login-card">
+          <div className="form-container">
+            <Form>
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="email" placeholder="Enter email"/>
+              <Form.Label className="form-margins">Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter password"/>
+              <Button variant="primary" type="submit" className="form-margins button-submit">
+                Accedi
+              </Button>
+              <p>Non hai un account? <Link to={"#"} className="registration-link">Registrati</Link></p>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
  
 export default Login;
